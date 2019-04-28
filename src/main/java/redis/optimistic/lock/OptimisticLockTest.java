c818a84cd44dc95ca757860c0b4e64bbe97b1879
tantan <tantan@zbj.com>
2019-04-28 15:25:46 +0800

    去掉多余的引用

--- src/main/java/redis/optimistic/lock/OptimisticLockTest.java
+++ src/main/java/redis/optimistic/lock/OptimisticLockTest.java
@@ -8,7 +8,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadFactory;
